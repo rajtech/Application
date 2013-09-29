570b46a8d0e3202c73dedc4eb4d8f7f6d69fdc79
Rajarajachozhan Natarajan <rajarajachozhan@gmail.com>
2013-09-29 08:53:40 +0100

    servlet adapter resource changed updated29/09/2013

--- ShoppingCart/src/java/com/rajtech/resources/AuthenticateResource.java
+++ ShoppingCart/src/java/com/rajtech/resources/AuthenticateResource.java
@@ -5,12 +5,16 @@
 package com.rajtech.resources;
 
 import com.rajtech.service.AuthCode;
+import com.rajtech.service.BasicAuthService;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
@@ -20,6 +24,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import org.apache.log4j.Logger;
 import org.joda.time.Period;
 
 /**
@@ -31,8 +36,14 @@ import org.joda.time.Period;
 public class AuthenticateResource {
 
     @Context
-    private UriInfo context;
-
+    HttpServletRequest request;
+    HttpServletResponse response;
+    HttpSession session;
+       private UriInfo context;
+    BasicAuthService bas = new BasicAuthService();
+    String output = null;
+    private URI location;
+     Logger logger = Logger.getLogger(AuthenticateResource.class.getName());
     /**
      * Creates a new instance of AuthenticateResource
      */
@@ -48,18 +59,20 @@ public class AuthenticateResource {
     public Response getUser() throws NoSuchAlgorithmException, UnsupportedEncodingException, IOException, ServletException, URISyntaxException {
         AuthCode ac = new AuthCode();
         System.out.println("Output from Server .... \n");
+        logger.debug("This is my first log message");  
         session = request.getSession();
         java.util.Date date = new java.util.Date();
         ac = bas.generator(request.getParameter("username"), Period.days(1));
         if (ac.getCode().isEmpty()) {
             request.setAttribute("errorMessage", "This is error!!");
-            request.getRequestDispatcher("report.jsp").forward(request, response);
+             location = new URI("http://localhost:8080/ShoppingCart/error.htm");
+            logger.debug("This is my first log message1");  
         } else {
             
             session.setAttribute("loginstatus", "Success");
             session.setAttribute("access_token", ac.getCode());
-            
-            location = new URI("http://localhost:8080/Accounts/admin/home.jsp");
+            logger.debug("This is my first log message2");  
+            location = new URI("http://localhost:8080/ShoppingCart");
           }
         return Response.temporaryRedirect(location).build();
     }
